1527641fb593bd98632383fc6539159a6bf7035d
Alhvi Balcarcel <abalcarcel@gmail.com>
2018-01-31 03:30:29 -0600

    se agregaron mas assets necesarios

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    SpriteRenderer sr;
+    public Camera cam;
+    private float speed = 5f;
+    private float jumpForce = 250f;
+    private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+        if (move != 0) {
+            rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
+            cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+            facingRight = move > 0;
+        }
+        
+        sr.flipX = !facingRight;
+
+        if (Input.GetButtonDown("Jump")) {
+            rb2d.AddForce(Vector2.up*jumpForce);
+        }
+
+	}
+}
