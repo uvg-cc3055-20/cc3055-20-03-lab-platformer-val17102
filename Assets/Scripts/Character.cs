b024494369e9696623554d6cceec3b06796b2ac1
val17102 <val17102@uvg.edu.gt>
2018-01-31 09:02:26 -0600

    Entrega  1
    Entrega 1

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,6 +7,8 @@ public class Character : MonoBehaviour {
 
     Rigidbody2D rb2d;
     SpriteRenderer sr;
+	Animator anim;
+	AudioSource audio;
     public Camera cam;
     private float speed = 5f;
     private float jumpForce = 250f;
@@ -17,6 +19,8 @@ public class Character : MonoBehaviour {
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+		anim = GetComponent<Animator> ();
+		audio = GetComponent<AudioSource> ();
     }
 	
 	// Update is called once per frame
@@ -32,7 +36,9 @@ public class Character : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump")) {
             rb2d.AddForce(Vector2.up*jumpForce);
+			audio.Play();
         }
+		anim.SetFloat ("Speed", Mathf.Abs (move));
 
 	}
 }
